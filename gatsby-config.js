5ac34746636ca2ee7a5b1ebf504391966cbc96a9
Joao Dumba <30002076@students.ual.pt>
2024-08-24 00:42:06 +0100

    Initial commit from gatsby

---
+++ gatsby-config.js
@@ -0,0 +1,10 @@
+/**
+ * @type {import('gatsby').GatsbyConfig}
+ */
+module.exports = {
+  siteMetadata: {
+    title: `Missão no Huambo`,
+    siteUrl: `https://www.yourdomain.tld`,
+  },
+  plugins: [],
+}
